cf99076360bf9f2d442413d49f1c9f6cf3c256c2
malikmahdivika <144976344+malikmahdivika@users.noreply.github.com>
2026-03-27 20:55:20 -0600

    REFACTOR: frontend made to call backend api deployment

--- allnourished-game/frontend/src/api.js
+++ allnourished-game/frontend/src/api.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://localhost:3001/api'
+const API_BASE = import.meta.env.VITE_API_URL;
 
 async function handleResponse(response) {
   const data = await response.json().catch(() => ({}))
